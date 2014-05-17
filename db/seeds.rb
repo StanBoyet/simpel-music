5bcc9360bb7b65a4424b9e86bb5bd8f02201226c
Stanislas BOYET <stanislas@boyet.me>
2014-05-17 23:14:06 +0200

    change seeds and header

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-types = Type.create([{ name: 'Link'} , { name: 'Audio'}, { name: 'Video'}, {name: 'Photo'}])
+types = Type.create([{ name: 'Soundcloud'} , { name: 'Youtube'}])
